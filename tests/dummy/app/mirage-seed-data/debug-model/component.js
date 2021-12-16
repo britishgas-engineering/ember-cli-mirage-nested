4cc4a4f1847545a7ccca6a8798b3e2f76a0ef8e0
Paul Ashwin Abrham <abrahamsam@Pauls-MacBook-Pro.local>
2021-12-16 19:26:19 +0530

    debug model glimmer

--- tests/dummy/app/mirage-seed-data/debug-model/component.js
+++ tests/dummy/app/mirage-seed-data/debug-model/component.js
@@ -1,46 +1,37 @@
-import { readOnly } from '@ember/object/computed';
-import EmberObject, { computed } from '@ember/object';
-import { on } from '@ember/object/evented';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import podNames from 'ember-component-css/pod-names';
 
-export default Component.extend({
-  initializeCollapsed: on('init', function () {
-    // otherwise it would be shared between different components
-    this.set('collapsed', EmberObject.create({}));
-    let model = this.model;
-    model.childrenAssociations.forEach((relName) => {
-      let rel = model[relName],
-        belongsTo = !rel || !rel.models,
-        model2 = belongsTo ? rel : rel.models[0];
-      if (model2 && model2.collapse) {
-        this.collapsed.set(relName, true);
-      }
-    });
-  }),
-  model: null,
-  parentModel: null,
-  relName: null,
-  collapsed: EmberObject.create({}),
-  level: 0,
-  levelNew: computed('level', function () {
+export default class DebugModelComponent extends Component {
+  get styleNamespace() {
+    return podNames['mirage-seed-data/debug-model'];
+  }
+
+  @tracked level = 0;
+  @tracked collapsed = {};
+
+  get levelNew() {
     return this.level + 1;
-  }),
-  flags: readOnly('model.forGUI.flags'),
-  error: null,
-  childrenAssociations: computed('model.childrenAssociations', function () {
-    let model = this.model;
+  }
+
+  get flags() {
+    return this.args.model.forGUI.flags;
+  }
+
+  get childrenAssociations() {
+    let model = this.args.model;
+
     if (model) {
       return model.childrenAssociations.map((relName) => {
         let rel = model[relName],
           belongsTo = !rel || !rel.models,
           hasMany = !belongsTo;
+
         return {
           name: relName,
-          count: belongsTo ? (rel ? 1 : 0) : rel.models.length,//eslint-disable-line
+          count: belongsTo ? (rel ? 1 : 0) : rel.models.length,
           models: belongsTo ? [rel] : rel.models,
-          /*collapseName: belongsTo ? (rel ? relName + rel.id : null) : rel.models.map((rel) => {//eslint-disable-line
-            return relName + rel.id;
-          })*/
           collapseName: relName,
           canAdd:
             (hasMany || !rel) &&
@@ -54,54 +45,51 @@ export default Component.extend({
     } else {
       return [];
     }
-  }),
-  actions: {
-    toggleCollapse(relName) {
-      let collapsed = this.collapsed;
-      collapsed.toggleProperty(relName);
-      return true; //for double click
-    },
-    addRelationship(relName) {
-      // console.log('addRelationship', relName, this.get('model'));
-      this.model.addRelationship(relName);
-      this.notifyPropertyChange('childrenAssociations');
-      this.send('refreshRoute');
-      return false;
-    },
-    deleteRelationship(relName, model, parentModel) {
-      // console.log('deleteRelationship', relName, model, parentModel);
-      parentModel.deleteRelationship(relName, model);
-      this.attrs.parentAssociationsHaveChanged();
-      return false;
-    },
-    associationsHaveChanged(propagate) {
-      this.notifyPropertyChange('childrenAssociations');
-      if (propagate) {
-        this.attrs.parentAssociationsHaveChanged();
+  }
+
+  @action toggleCollapse(relName) {
+    let collapsed = this.collapsed;
+    collapsed[relName] = !collapsed[relName];
+  }
+
+  @action addRelationship(relName) {
+    this.args.model.addRelationship(relName);
+    this.refreshRoute();
+  }
+
+  @action deleteRelationship(relName, model, parentModel) {
+    parentModel.deleteRelationship(relName, model);
+    this.args.parentAssociationsHaveChanged();
+  }
+
+  @action associationsHaveChanged(propagate) {
+    if (propagate) {
+      this.args.parentAssociationsHaveChanged();
+    } else {
+      this.refreshRoute();
+    }
+  }
+
+  @action changeFlag(flag, option) {
+    let model = this.args.model;
+    try {
+      if (model[flag.method]) {
+        model[flag.method](option);
       } else {
-        this.send('refreshRoute');
+        model.update(flag.name, option);
       }
-    },
-    changeFlag(flag, option) {
-      let model = this.model;
-      try {
-        if (model[flag.method]) {
-          model[flag.method](option);
-        } else {
-          model.update(flag.name, option);
-        }
-      } catch (err) {
-        this.send('sendError', err);
-      }
-      this.notifyPropertyChange('childrenAssociations');
-      this.attrs.parentAssociationsHaveChanged(true); //when changing servicesProductHolding hasProduct for example
-      this.send('refreshRoute');
-    },
-    refreshRoute() {
-      return this.attrs.refreshRoute();
-    },
-    sendError(err) {
-      return this.attrs.sendError(err);
-    },
-  },
-});
+    } catch (err) {
+      this.send('sendError', err);
+    }
+    this.args.parentAssociationsHaveChanged(true); //when changing servicesProductHolding hasProduct for example
+    this.refreshRoute();
+  }
+
+  @action refreshRoute() {
+    this.args.refreshRoute();
+  }
+
+  @action sendError(err) {
+    this.args.sendError(err);
+  }
+}
