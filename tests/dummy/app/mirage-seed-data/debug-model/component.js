379ff282e2799900de4f1a73b0b7eec518c27daa
Paul Ashwin Abrham <abrahamsam@Pauls-MacBook-Pro.local>
2021-12-17 14:14:59 +0530

    Route changes

--- tests/dummy/app/mirage-seed-data/debug-model/component.js
+++ tests/dummy/app/mirage-seed-data/debug-model/component.js
@@ -11,6 +11,22 @@ export default class DebugModelComponent extends Component {
   @tracked level = 0;
   @tracked collapsed = {};
 
+  constructor(owner, args) {
+    super(owner, args);
+
+    const model = this.args.model;
+
+    model.childrenAssociations.forEach((relName) => {
+      const rel = model[relName];
+      const belongsTo = !rel || !rel.models;
+      const model2 = belongsTo ? rel : rel.models[0];
+
+      if (model2 && model2.collapse) {
+        this.collapsed[relName] = true;
+      }
+    });
+  }
+
   get levelNew() {
     return this.level + 1;
   }
@@ -20,14 +36,13 @@ export default class DebugModelComponent extends Component {
   }
 
   get childrenAssociations() {
-    let model = this.args.model;
+    const model = this.args.model;
 
     if (model) {
       return model.childrenAssociations.map((relName) => {
         let rel = model[relName],
           belongsTo = !rel || !rel.models,
           hasMany = !belongsTo;
-
         return {
           name: relName,
           count: belongsTo ? (rel ? 1 : 0) : rel.models.length,
@@ -48,8 +63,9 @@ export default class DebugModelComponent extends Component {
   }
 
   @action toggleCollapse(relName) {
-    let collapsed = this.collapsed;
-    collapsed[relName] = !collapsed[relName];
+    const newRelName = {};
+    newRelName[relName] = !this.collapsed[relName];
+    this.collapsed = { ...this.collapsed, ...newRelName };
   }
 
   @action addRelationship(relName) {
@@ -81,7 +97,7 @@ export default class DebugModelComponent extends Component {
     } catch (err) {
       this.send('sendError', err);
     }
-    this.args.parentAssociationsHaveChanged(true); //when changing servicesProductHolding hasProduct for example
+    this.args.parentAssociationsHaveChanged(true);
     this.refreshRoute();
   }
 
