1b6ed218d65893f677ed479d5f7ac7050a645252
Paul Ashwin Abrham <abrahamsam@Pauls-MacBook-Pro.local>
2022-01-12 22:42:46 +0530

    Class based component

--- tests/dummy/mirage/models/bg-model.js
+++ tests/dummy/mirage/models/bg-model.js
@@ -1,4 +1,4 @@
 import { Model } from 'ember-cli-mirage';
 import EmberCliMirageNestedMixin from 'ember-cli-mirage-nested/mirage/bg-model-mixin';
 
-export default Model.extend(EmberCliMirageNestedMixin);
+export default class BgModel extends Model.extend(EmberCliMirageNestedMixin) {}
